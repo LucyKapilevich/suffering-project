7836be96ace5990ccdfc658686475d7ef138fa8f
Leon <leonmthistle@gmail.com>
2025-01-22 19:46:01 +0100

    Added religion/death cause sorter to occupation sorter

--- Python Group/occupation.py
+++ Python Group/occupation.py
@@ -50,18 +50,6 @@ for filename in os.listdir():
                 
                 lifespan = deathyear - birthyear
 
-                # if isinstance(religion, list):
-                #     for rel in religion:
-                #         if not any(char in rel for char in '_:.,;-1234567890'):
-                #             religion_dict[rel] = {}
-                #             religion_dict[religion].append(lifespan)
-                # elif isinstance(religion, str):
-                #     if not any(char in religion for char in '_:.,;-1234567890'):
-                #         religion_dict[religion] = 
-                #         religion_dict[religion].append(lifespan)
-                # else:
-                #     religion = None
-
                 # Creates occupation dictionary to count all occupation entries
                 if isinstance(occupation, list):
                     for occ in occupation:
@@ -75,11 +63,50 @@ for filename in os.listdir():
                             occupation_dict[occupation] = {"Lifespan": []}
                         occupation_dict[occupation]["Lifespan"].append(lifespan)
 
-print(occupation_dict)
+                # Creates religion dictionary to count all religion entries
+                if isinstance(religion, list):
+                    for rel in religion:
+                        if not any(char in rel for char in '_:.,;-1234567890'):
+                            if rel not in religion_dict:
+                                religion_dict[rel] = {"Lifespan": []}
+                            religion_dict[rel]["Lifespan"].append(lifespan)
+                elif isinstance(religion, str):
+                    if not any(char in religion for char in '_:.,;-1234567890'):
+                        if religion not in religion_dict:
+                            religion_dict[religion] = {"Lifespan": []}
+                        religion_dict[religion]["Lifespan"].append(lifespan)
+                                # Creates religion dictionary to count all religion entries
 
-# Store data in csv file
+                # Creates deathcause dictionary to count all deathcause entries
+                if isinstance(deathcause, list):
+                    for rel in deathcause:
+                        if not any(char in rel for char in '_:.,;-1234567890'):
+                            if rel not in deathcause_dict:
+                                deathcause_dict[rel] = {"Lifespan": []}
+                            deathcause_dict[rel]["Lifespan"].append(lifespan)
+                elif isinstance(deathcause, str):
+                    if not any(char in deathcause for char in '_:.,;-1234567890'):
+                        if deathcause not in deathcause_dict:
+                            deathcause_dict[deathcause] = {"Lifespan": []}
+                        deathcause_dict[deathcause]["Lifespan"].append(lifespan)
+
+# Store occupation in csv file
 with open('../occupation_data.csv', 'w', encoding='utf-8', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=["Occupation", "Lifespans"])
     writer.writeheader()
     for occupation, data in occupation_dict.items():
         writer.writerow({"Occupation": occupation, "Lifespans": data["Lifespan"]})
+
+# Store religion in csv file
+with open('../religion_data.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["Occupation", "Lifespans"])
+    writer.writeheader()
+    for religion, data in religion_dict.items():
+        writer.writerow({"Occupation": religion, "Lifespans": data["Lifespan"]})
+
+# Store deathcause in csv file
+with open('../deathcause_data.csv', 'w', encoding='utf-8', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["Occupation", "Lifespans"])
+    writer.writeheader()
+    for deathcause, data in deathcause_dict.items():
+        writer.writerow({"Occupation": deathcause, "Lifespans": data["Lifespan"]})
